a1ef5aa92125c366eff766a265023983cb7be766
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-04-07 15:34:24 +1000

    modified first functional comp with use of props to make it output dynamic content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       Hi, I am react App!
+      <h1> Hi, I am react App!</h1>
+      <Person name = "Amit" age = "32" />
+      <Person name = "Sai" age = "28"/>
+      <Person name = "Aveer" age = "5"> I am a child </Person>
+
       </div>
     );
   }
