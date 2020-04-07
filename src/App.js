e60bf8ecbba5001a6c68753157e5502373947a32
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-04-07 20:19:27 +1000

    converted APP component in useState hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
-    person: [
-      {name: 'Amit', age: '32'},
-      {name: 'Sai', age: '28'},
-      {name: 'Aveer', age: '5'}
-    ],
-    otherData: 'I am a child '
-  }
+const App = (props) => {
+  const [personState,setPersonState] = useState({person: [
+    {name: 'Amit', age: '32'},
+    {name: 'Sai', age: '28'},
+    {name: 'Aveer', age: '5'}
+  ]});
 
-  stateNameHandler = () => {
-    this.setState( {
+  const [otherState,setOtherState] = useState('I am a child');
+  
+  console.log(personState , otherState);
+
+  const stateNameHandler = () => {
+    setPersonState( {
       person : [
         {name: 'Amit Ashtekar', age: '32'},
         {name: 'Sai', age: '28'},
@@ -21,18 +22,18 @@ class App extends Component {
       ]
     })
   }
-  render() {
+  
     return (
       <div className="App">
-      <button onClick = {this.stateNameHandler}>state name</button>
+      <button onClick = {stateNameHandler}>state name</button>
       <h1> Hi, I am react App!</h1>
-      <Person name = {this.state.person[0].name} age = {this.state.person[0].age} />
-      <Person name = {this.state.person[1].name} age = {this.state.person[1].age}/>
-      <Person name = {this.state.person[2].name} age = {this.state.person[2].age}> {this.state.otherData}</Person>
+      <Person name = {personState.person[0].name} age = {personState.person[0].age} />
+      <Person name = {personState.person[1].name} age = {personState.person[1].age}/>
+      <Person name = {personState.person[2].name} age = {personState.person[2].age}> {otherState}</Person>
 
       </div>
     );
-  }
+
 }
 
 export default App;
