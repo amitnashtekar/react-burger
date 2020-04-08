0990a13ca73bcc364581ae1ff457249fd8f17121
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-04-08 13:10:17 +1000

    conditional rendering, use of keys, passing methods to delete and update state

--- src/App.js
+++ src/App.js
@@ -5,51 +5,63 @@ import './App.css';
 class App extends Component {
   state = {
     person: [
-      {name: 'Amit', age: '32'},
-      {name: 'Sai', age: '28'},
-      {name: 'Aveer', age: '5'}
+      {id: 'vhj4', name: 'Amit', age: '32'},
+      {id: 'vhj33', name: 'Sai', age: '28'},
+      {id: 'vhj754', name: 'Aveer', age: '5'}
     ],
-    otherData: 'I am a child '
+    otherData: 'I am a child ',
+    showPerson: false
   }
 
-  stateNameHandler = () => {
-    this.setState( {
-      person : [
-        {name: 'Amit Ashtekar', age: '32'},
-        {name: 'Sai', age: '28'},
-        {name: 'Aveer', age: '6'}
-      ]
-    })
+  togglePersons = () => {
+    let showPerson = this.state.showPerson;
+    this.setState({showPerson: !showPerson});
   }
 
-  changeNameHandler = (event) => {
+  changeNameHandler = (id, event) => {
+    console.log(event, id);
+    const personIndex = this.state.person.findIndex(p => {
+      return p.id === id
+    });
+    const person = {
+      ...this.state.person[personIndex]
+    }
+    person.name = event.target.value;
+    const persons = [...this.state.person];
+    persons[personIndex] = person;
     this.setState( {
-      person : [
-        {name: 'Amit Ashtekar', age: '32'},
-        {name: event.target.value, age: '28'},
-        {name: 'Aveer', age: '6'}
-      ]
+      person : persons
     })
   }
+  
+  deletePersonHandlr = (index) => {
+    console.log('index', index);
+    const newPersonState = [...this.state.person];
+    newPersonState.splice(index, 1);
+    this.setState({person: newPersonState});
+  }
+
   render() {
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {
+            this.state.person.map((person, index) => {
+              return <Person name = {person.name} age = {person.age}
+              delete = {this.deletePersonHandlr.bind(this, index)}
+              change = {this.changeNameHandler.bind(this, person.id)}
+              key = {person.id} />
+            })
+          }
+        </div>
+      )
+    }
     return (
       <div className="App">
-      <button onClick = {this.stateNameHandler}>state name</button>
+      <button onClick = {this.togglePersons}>state name</button>
       <h1> Hi, I am react App!</h1>
-      <Person 
-      name = {this.state.person[0].name}
-      age = {this.state.person[0].age}
-       />
-      <Person 
-      name = {this.state.person[1].name}
-      age = {this.state.person[1].age}
-      change = {this.changeNameHandler}
-      />
-      <Person 
-      name = {this.state.person[2].name}
-      age = {this.state.person[2].age}> 
-        {this.state.otherData}
-      </Person>
+      {persons}
 
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Person = (props) => {
     return (
         <div>
-            <p >I am person ! my name is {props.name} and age is {props.age}</p>
+            <p onClick = {props.delete}>I am person ! my name is {props.name} and age is {props.age}</p>
             <p>{props.children}</p>
             <input type = 'text' onChange = {props.change} value = {props.name}></input>
         </div>
