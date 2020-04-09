ea59384ec8b74a42ca588915a51ebd0bec4b27b3
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-04-09 15:50:42 +1000

    use of CSS Modules with media query

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('classes', classes);
     const style = {
       backgroundColor: 'green',
       color: 'white',
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Radium from 'radium';
+import classes from './Person.css';
 
 
 const Person = (props) => {
-    const style = {
-        '@media (min-width:500px)': {
-            backgroundColor: 'yellow',
-            width: '50%'
-        }
-    }
+    // const style = {
+    //     '@media (min-width:500px)': {
+    //         backgroundColor: 'yellow',
+    //         width: '50%'
+    //     }
+    // }
     return (
-        <div style = {style}>
+        <div className = {classes.Person}>
             <p onClick = {props.delete}>I am person ! my name is {props.name} and age is {props.age}</p>
             <p>{props.children}</p>
             <input type = 'text' onChange = {props.change} value = {props.name}></input>
@@ -19,4 +20,4 @@ const Person = (props) => {
     )
 }
 
-export default Radium(Person);
+export default Radium(Person);//we have used Radium HOC still CSS module is working.
