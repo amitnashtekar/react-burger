d18db5fee11629692ecb029e78baebcdebfcca83
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-04-21 00:10:34 +1000

    passing required props from parent cmpt to nested cmpt via render attribute of Route and order submission

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -1,26 +1,63 @@
 import React, {Component} from 'react';
 import Button from '../../../Components/UI/Button/Button';
+import Spinner from '../../../Components/UI/Spinner/Spinner';
 import CDClaases from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
-    state = {
-        
-                    name: 'Amit Ashekar',
-                    address: {
-                        street: 'test street 1',
-                        zipCode: '43256',                        
-                    },
-                    email: 'abc@test.com'
-            
+    state = {        
+                name: 'Amit Ashekar',
+                address: {
+                    street: 'test street 1',
+                    zipCode: '43256',                        
+                },
+                email: 'abc@test.com',
+                loading: false            
+    }
+    orderHandler = () => {
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Amit Ashekar',
+                address: {
+                    street: 'test street 1',
+                    zipCode: '43256',
+                    country: 'Australia'
+                },
+                email: 'abc@test.com'
+            },
+            deliveryMethod: 'fast'
+        }
+        axios.post('/order.json', order)
+        .then(response => {
+            console.log(response);
+            this.setState({loading: false});
+            this.props.history.push('/');
+        })
+        .catch(error => {
+            console.log(error);
+            this.setState({loading: false});
+        });
     }
     render() {
-        return(
-            <div className = {CDClaases.ContactData}>
+        let form = (
+            <React.Fragment>
                 <input type = "text" name="name" placeholder="name" />
                 <input type = "email" name="street" placeholder="street" />
                 <input type = "street" name="name" placeholder="street" />
                 <input type = "zipcode" name="zipcode" placeholder="zipcode" />
-                <Button type = "Success" >Order</Button>                
+                <Button type = "Success"
+                clicked = {this.orderHandler}  >Order</Button>
+            </React.Fragment>
+        )
+        if (this.state.loading) {
+            form = <Spinner />
+        }
+        return(
+            <div className = {CDClaases.ContactData}>
+                {form}               
             </div>
         )
     }
