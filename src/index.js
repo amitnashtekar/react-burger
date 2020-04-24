9e31f5a9c89c462ca4911a6efcf03e09499a3b81
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2020-04-24 19:10:03 +1000

    added redux devtool and logger middleware

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import reducer from '../src/store/reducer';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware, compose} from 'redux';
 
 
-const store = createStore(reducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[MIDDLEWARE] dispatching action',action);
+            const result = next(action);
+            console.log('[MIDDLEWARE] next state', store.getState());
+            return result;
+        }
+    }
+}
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+const store = createStore(reducer, composeEnhancer(applyMiddleware(logger)));
 
 const app = (
     <Provider store = {store} >
